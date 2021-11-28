bb4acbf6bca83986269950eee5bcd0a3a9802f3a
snackfurt <deingesichtundmeinarsch@web.de>
2021-11-28 17:55:15 +0100

    initial commit

---
+++ src/modules/Waveform.svelte
@@ -0,0 +1,22 @@
+<script>
+    import * as Pizzicato from 'pizzicato';
+
+    const sawtoothWave = new Pizzicato.Sound({
+        source: 'wave',
+        options: {
+            type: 'sawtooth'
+        }
+    });
+
+    function startSound() {
+        sawtoothWave.play();
+    }
+
+    function stopSound() {
+        sawtoothWave.stop();
+    }
+</script>
+
+<button on:mousedown={startSound} on:mouseup={stopSound}>
+    make a sound
+</button>
