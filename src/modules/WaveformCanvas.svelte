a57b6952d360337a0f40d40838ad4f9765823be3
snackfurt <deingesichtundmeinarsch@web.de>
2021-11-29 19:30:36 +0100

    use image and offscreen canvas to display wave (no resetting of image, nice overlay effect)

--- src/modules/WaveformCanvas.svelte
+++ src/modules/WaveformCanvas.svelte
@@ -1,3 +1,8 @@
+<div class="canvasContainer">
+    <canvas bind:this={onscreenCanvas} width={600} height={200}></canvas>
+</div>
+
+
 <script>
     import { afterUpdate, onMount } from 'svelte';
     import { createAnalyser } from '../utils/wave-utils';
@@ -5,71 +10,92 @@
     export let doDrawing;
     export let soundWave;
 
-    let canvas;
-    let drawingContext;
+    let onscreenCanvas;
+    let onscreenDrawingContext;
+    let offscreenCanvas;
+    let offscreenDrawingContext;
     let getSoundWaveData;
+
+    let canvasImage;
+    let canvasImageSource;
 
     onMount(() => {
         init();
     });
     afterUpdate(() => {
-        drawNext();
+        drawNextWaveform();
     });
 
     function init() {
         const { getWaveData, waveformLength } = createAnalyser(soundWave);
 
-        canvas.width = waveformLength;
-        canvas.height = canvas.width * 0.33;
+        offscreenCanvas = document.createElement('canvas');
+        offscreenCanvas.width = waveformLength;
+        offscreenCanvas.height = offscreenCanvas.width * 0.33;
 
-        drawingContext = canvas.getContext('2d');
-        drawingContext.strokeStyle = "#ff3e00";
+        offscreenDrawingContext = offscreenCanvas.getContext('2d');
+        offscreenDrawingContext.strokeStyle = "#ff3e00";
 
         getSoundWaveData = getWaveData;
+
+        canvasImage = new Image;
+        canvasImageSource = offscreenCanvas.toDataURL();
+
+        onscreenDrawingContext = onscreenCanvas.getContext('2d');
     }
 
-    function draw() {
+    function drawWaveform() {
         const waveData = getSoundWaveData();
         const { x: startX, y: startY } = getWavePointCoordsAtIndex(waveData, 0);
 
-        drawingContext.clearRect(0, 0, canvas.width, canvas.height);
-        drawingContext.beginPath();
-        drawingContext.moveTo(startX, startY);
+        offscreenDrawingContext.clearRect(0, 0, offscreenCanvas.width, offscreenCanvas.height);
+        offscreenDrawingContext.beginPath();
+        offscreenDrawingContext.moveTo(startX, startY);
         for (let i = 1; i < waveData.length; i++) {
             const { x, y } = getWavePointCoordsAtIndex(waveData, i);
-            drawingContext.lineTo(x, y);
+            offscreenDrawingContext.lineTo(x, y);
         }
-        drawingContext.stroke();
+        offscreenDrawingContext.stroke();
 
-        drawNext();
+        renderImage();
+
+        drawNextWaveform();
     }
 
-    function drawNext() {
+    function drawNextWaveform() {
         if (doDrawing) {
-            requestAnimationFrame(draw);
+            requestAnimationFrame(drawWaveform);
         }
     }
 
     function getWavePointCoordsAtIndex(waveData, index) {
         const padding = 100;   // avoid clipping of top points
         const yOffset = padding / 2;
-        const height = canvas.height - padding;
+        const height = offscreenCanvas.height - padding;
         return {
             x: index,
             y: yOffset + (0.5 + waveData[index] / 2) * height,
         }
     }
 
+    function renderImage() {
+        canvasImage.onload = () => {
+            //if the image is being drawn due to resizing, reset the width and height. Putting the width and height outside the canvasImage.onload function will make scaling smoother, but the image will flicker as you scale. Pick your poison.
+            //onscreenCanvas.width = offscreenCanvas.width;
+            //onscreenCanvas.height = offscreenCanvas.height;
+            //Prevent blurring
+            onscreenDrawingContext.imageSmoothingEnabled = false;
+            onscreenDrawingContext.drawImage(canvasImage,0,0,onscreenCanvas.width,onscreenCanvas.height)
+        }
+
+        canvasImage.src = offscreenCanvas.toDataURL();;
+    }
+
 </script>
-
-
-<div class="canvasContainer">
-    <canvas bind:this={canvas} width={32} height={32}></canvas>
-</div>
 
 
 <style>
     .canvasContainer {
-        max-width: 200px;
+        width: 100%;
     }
 </style>
